b77c6900c3533c51e8894b40428e7a1db92dcb36
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-02-22 22:56:41 +0100

    project current final state

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,7 +3,7 @@ const API_URL = "https://api.adviceslip.com/advice"
 export const getAdvice =  async () => {
     try {
         const advice = await (await fetch(API_URL)).json()
-        return advice
+        return advice.slip
     }
     catch(err) {
         console.error(err)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,41 @@
 import { getAdvice } from './api.js'
+const adviceDisplay = document.querySelector('.advice__content p')
+const adviceIdDisplay = document.querySelector("#advice_id")
+const adviceBtn = document.getElementById("new_advice_btn")
 
-const advice = await getAdvice()
-console.log(advice)
+newAdvice()
+
+adviceBtn.addEventListener("click", newAdvice)
+
+
+function setContent({advice, id}) {
+
+    setOpacity([adviceDisplay, adviceIdDisplay], 0)
+
+    adviceDisplay.addEventListener("transitionend", () => {
+        adviceDisplay.textContent = advice
+        setOpacity([adviceDisplay], 1)
+    })
+
+    adviceIdDisplay.addEventListener("transitionend", () => {
+        adviceIdDisplay.textContent = `#${id}`
+        setOpacity([adviceIdDisplay], 1)
+    })
+    
+}
+
+function setOpacity(arr, value) {
+    arr.forEach(element => {
+        element.style.opacity = value
+    })
+}
+
+async function newAdvice() {
+    try {
+        const advice = await getAdvice()
+        setContent(advice)
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
